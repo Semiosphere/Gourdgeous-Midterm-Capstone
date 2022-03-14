1f3cbece5d0b7b5902c23f66abfcf54096d413b2
Zach Dugger <ztd522@gmail.com>
2022-03-14 16:53:36 -0400

    got user created avatars to appear on a users page

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { AvatarForm } from "./builder/Builder"
+import { AvatarList } from "./users/MyAvatars"
 
 
 
@@ -9,6 +10,10 @@ export const ApplicationViews = () => {
         <>
             <Route exact path="/avatars/create">
                 <AvatarForm />
+            </Route>
+
+            <Route exact path="/MyAvatars">
+                <AvatarList />
             </Route>
         </>
     )
